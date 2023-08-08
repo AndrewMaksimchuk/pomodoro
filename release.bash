602d2b0f0eb8528bb885b3c261b2f0c9f69042a1
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2023-08-08 15:06:19 +0300

    add release script

---
+++ release.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+echo "[ Build application... ]"
+make build
+
+echo "[ Enter new release version - major.minor.patch ]"
+read version
+
+echo "[ Installing a new version and tag ]"
+npm version $version
+
+tag="v$version"
+
+echo "[ Update repository ]"
+git push origin main
+git push origin $tag
+
+echo "[ Create project release ]"
+
+echo "[ Enter release note ]"
+note_file="release_note"
+date +"%d.%m.%Y" > ./$note_file
+echo >> ./$note_file
+vim -f $note_file
+note=$(cat ./$note_file)
+
+gh release create $tag \
+  --title "$tag" \
+  --notes "$note"
+
+linux=$(echo ./out/make/deb/x64/*.deb)
+
+gh release upload $tag $linux
+
+echo "[ Done ]"
