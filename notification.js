60f1f13b04cbdd12343d0f7a7e28f7cb9b124816
Andrew Maksimchuk <32966111+AndrewMaksimchuk@users.noreply.github.com>
2024-10-11 22:38:15 +0300

    remove *.js files, ledt only .mjs and .ts

--- notification.js
+++
@@ -1,9 +0,0 @@
-import { Notification } from "electron";
-export function showNotification(title, body) {
-    const notification = new Notification({
-        title,
-        body,
-    });
-    notification.show();
-    notification.on("show", () => notification.close());
-}
