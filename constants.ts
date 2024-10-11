7729d9ffd47244323b8a17c379bb3f238c1f321e
Andrew Maksimchuk <32966111+AndrewMaksimchuk@users.noreply.github.com>
2024-10-11 21:32:23 +0300

    config typescript

--- constants.ts
+++ constants.ts
@@ -1,13 +1,16 @@
 import { app } from "electron";
+
 export const ONEMINUTE = 60 * 1000;
 export const LONGTIME = ONEMINUTE * 25;
 export const BREAKETIME = ONEMINUTE * 5;
 export const TRAYCOUNTER = 5;
 export const TRAYCOUNTERTIME = LONGTIME - ONEMINUTE * (TRAYCOUNTER + 1);
-let USER_DATA = undefined;
+let USER_DATA: string | undefined = undefined;
+
 export function setOsConstants() {
-    USER_DATA = app.getPath("userData");
+  USER_DATA = app.getPath("userData");
 }
+
 export function getUserData() {
-    return USER_DATA;
+  return USER_DATA;
 }
